eef45d53f1087e6abf45dfdd0b6c1f5976efd28a
mmd <mmd@gmail.com>
2025-05-19 21:29:28 +0430

    create and start project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold text-center text-white bg-red-800"> Hello world! </h1>
+    </>
+  );
+}
+
+export default App;
