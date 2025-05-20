9f1a6ad316b3251111e801aa7947c77249ebe20d
amin <amin@gmail.com>
2025-05-21 01:19:51 +0430

    change routes page

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,3 +1,11 @@
-const router = [
-    {path:"",element:{}}
+import Home from "./pages/Home";
+import LoginRegister from "./pages/LoginRegister";
+import Page404 from "./pages/Page404";
+
+let routes = [
+    { path: "/", element: <Home /> },
+    { path: "/auth", element: <LoginRegister /> },
+    { path: "*", element: <Page404 /> },
 ]
+
+export default routes
