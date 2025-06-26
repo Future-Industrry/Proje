9a3447fe8689df854b17f059d4516946c6b0cec1
hani <hanirezaee496@gmail.com>
2025-06-26 22:49:38 +0330

    import component Profile to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useRoutes } from "react-router-dom";
 import "./App.css";
 import routes from "./routes";
+import Profile from "./components/Profile";
 
 function App() {
   let routers = useRoutes(routes)
-  return routers
+  return (
+    <div>
+      <Profile></Profile>
+    </div>
+  )
 }
 
 export default App;
