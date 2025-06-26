361a5149b9ef8c5a76bc7eaba67aef4f28fbe865
hani <hanirezaee496@gmail.com>
2025-06-26 23:01:43 +0330

    import routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useRoutes } from "react-router-dom";
+import { useRoutes,BrowserRouter,Routes,Route } from "react-router-dom";
 import "./App.css";
 import routes from "./routes";
-import Profile from "./components/Profile";
+import ProfileAbout from "./pages/ProfileAbout";
+import ProfileCollection from "./pages/ProfileCollection";
+import ProfileSample from "./pages/ProfileSample";
 
 function App() {
   let routers = useRoutes(routes)
   return (
-    <div>
-      <Profile></Profile>
-    </div>
+    <BrowserRouter>
+            <Routes>
+                <Route path='/profile/about' element={<ProfileAbout/>}/>
+                <Route path='/profile/collection' element={<ProfileCollection/>}/>
+                <Route path='/profile/sample' element={<ProfileSample/>}/>
+            </Routes>
+        </BrowserRouter>
   )
 }
 
