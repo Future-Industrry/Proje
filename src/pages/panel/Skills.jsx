0b00da2c369c940becf5bf11179308cf5d8409aa
hani <hanirezaee496@gmail.com>
2025-07-13 11:42:38 +0330

    improving...

--- src/pages/panel/Skills.jsx
+++ src/pages/panel/Skills.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import SideBar from '../../components/SideBar';
 import TopBar from '../../components/TopBar';
-import Edit from '../../assets/images/edit-2.svg'
-import User from '../../assets/images/user.svg'
+import { CiEdit } from "react-icons/ci";
+import { LiaUserSolid } from "react-icons/lia";
 export default function Skills() {
   const [isOpen, setIsOpen] = useState(0)
   return (
@@ -15,13 +15,13 @@ export default function Skills() {
           <div className="p-5">
             {/*کداتو اینجا بزن*/}
             <div className='border-1 border-light grid grid-cols-2 py-6 px-10 gap-7 rounded-3xl sm:gap-y-7 shadow text-sm sm:gap-x-0 sm:grid-cols-[18%_18%_18%_45%]'>
-              <div className='flex gap-2 sm:col-span-2'>
+              <div className='flex items-center gap-2 sm:col-span-2'>
+                <LiaUserSolid className="sm:size-[2.2rem] size-7" />
                 <p className=' font-IranYekanBold sm:text-lg'>مهارت های فردی </p>
-                <img src={User} alt="" />
               </div>
-              <div className='flex gap-2 sm:col-span-2 border w-max justify-self-end px-3 py-1 rounded-lg '>
+              <div className='flex  gap-2 sm:col-span-2 border w-max justify-self-end px-3 py-1 rounded-lg '>
                 <p className=''>ویرایش</p>
-                <img src={Edit} alt="" />
+                <CiEdit className="size-[1.2rem]" />
               </div>
               <p className='sm:border-b-1 border-second-light text-right sm:pb-4'>PYTHON</p>
               <p className="text-second-light text-left sm:text-right sm:border-b-1 sm:border-second-light sm:pb-4">100%</p>
