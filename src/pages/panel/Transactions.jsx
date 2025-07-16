1fd02aedd24bdda273dde71838e47b2765537c42
MohamadAminPour <ytornado37@gmail.com>
2025-07-16 14:02:53 +0430

    add some change

--- src/pages/panel/Transactions.jsx
+++ src/pages/panel/Transactions.jsx
@@ -4,6 +4,15 @@ import TopBar from '../../components/TopBar';
 
 export default function Transactions() {
   const [isOpen, setIsOpen] = useState(0)
+  const transactions = [
+    {
+      id: 0,
+      description: "خرید",
+      time: "۵ تیر ۱۴۰۳ ساعت 00:38",
+      amount: "0",
+      status: "درحال انتظار",
+    },
+  ];
   return (
     <>
       <div className="flex *:h-[100vh] ">
@@ -13,7 +22,62 @@ export default function Transactions() {
           <TopBar isOpen={isOpen} setIsOpen={setIsOpen}/>
           <div className="p-5">
             {/*کداتو اینجا بزن*/}
-            <p>Transactions</p>
+
+            <div className="max-w-5xl mx-auto p-6 font-yekan text-right">
+    
+
+      {/* کیف پول */}
+      <div className="mt-12">
+        <h3 className="text-[16px] md:text-[32px] leading-7 dark:text-[#ffff]  font-bold mb-[20px]">کیف پول</h3>
+        <div className="border-[1px] border-[#CDCACA] mb-8 mt-10 w-full "></div>
+
+    
+          <div className="flex gap-2 items-baseline">
+                <p className="text-[16px] md:text-[32px] leading-7 font-medium dark:text-[#ffff]">
+          موجودی:
+            </p>
+             <p className=" w-[120px] h-[47px] rounded-[8px] text-center pt-2 bg-[#E8E8E8] dark:bg-[#242424] text-[12px] md:text-[22px] dark:text-[#AAAAAA] leading-[30px] text-[#5D5D5D] font-extrabold">۰ تومان</p>
+          </div>
+        
+      </div>
+
+      {/* تراکنش‌ها */}
+      <div className="mt-[90px]">
+        <h3 className="text-[14px] md:text-[32px] leading-7 font-medium dark:text-[#ffff]  mb-[30px]">تراکنش‌ها</h3>
+        {/* عنوان ستون‌ها */}
+        <div className="border-[1px] border-[#CDCACA] mb-6  w-full "></div>
+        <div className="bg-[#EEEBEB] dark:bg-[#211E1E] rounded-t-lg p-3 py-6 grid grid-cols-6 justify-items-center gap-4 dark:text-[#ffff]  text-[16px] md:text-[26px] leading-7 font-bold text-black">
+          <span>شناسه</span>
+          <span>مبلغ</span>
+          <span className="col-span-2">زمان</span>
+          <span>توضیحات</span>
+          <span>وضعیت</span>
+        </div>
+
+        {/* لیست تراکنش‌ها */}
+        {transactions.map((tx) => (
+          <div
+            key={tx.id}
+            className="w-full bg-white shadow-md dark:bg-[#1B202C] rounded-b-lg px-6 py-8  grid grid-cols-6 justify-items-center gap-4 dark:text-[#ffff] text-black text-[12px] md:text-[24px]"
+          >
+            <div>{tx.id}</div>
+            <div>{tx.amount} تومان</div>
+            <div className="col-span-2">{tx.time}</div>
+            <div>{tx.description}</div>
+            <div>
+              <p className="bg-[#FBBD08] text-center text-white px-1.5  md:px-4 py-1 rounded-[8px] text-[12px] md:text-[18px]">
+                {tx.status}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+     
+    
+
+  
+      
           </div>
         </div>
       </div>
