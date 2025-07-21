a43db858ae2c21fcbbf88813588b7f23b1385e2c
amene bakhshian <amenehbakhshiane@gmail.com>
2025-07-22 03:26:36 +0330

    added Projects

--- src/pages/panel/Projects.jsx
+++ src/pages/panel/Projects.jsx
@@ -5,6 +5,14 @@ import { BsSun, BsMoon } from 'react-icons/bs';
 import { useNavigate } from "react-router-dom";
 import { BsBookmarkFill } from "react-icons/bs";
 
+import projeimg from "../../assets/images/projeimg.png"
+ import { AiOutlinePlus } from "react-icons/ai";
+ import { SiCodecrafters } from "react-icons/si";
+ import { CiCirclePlus } from "react-icons/ci";
+ import { FaGithub } from "react-icons/fa";
+ import { BiEditAlt } from "react-icons/bi";
+ import { IoIosCloseCircleOutline } from "react-icons/io";
+
 
 export default function Projects() {
   const [isOpen, setIsOpen] = useState(0);
@@ -18,8 +26,61 @@ export default function Projects() {
           <div className={`bg-dark/50 w-full absolute ${isOpen ? 'h-full' : "h-[0]"} `} onClick={() => setIsOpen(!isOpen)}></div>
           <TopBar isOpen={isOpen} setIsOpen={setIsOpen}/>
           <div className="p-5">
-            {/*کداتو اینجا بزن*/}
-            <p>Projects</p>
+          
+          <div className="p-4 space-y-6 bg-white dark:bg-[#02142C] w-auto">
+      {/* پروژه های متن باز */}
+      <div className="border-2 border-[#EEEBEB] dark:border-[#1B202C] dark:bg-[#1B202C] border-solid">
+          <div className="bg-[#EEEBEB] dark:bg-[#211E1E] p-4 flex justify-between items-center  border-solid">
+            <div className="flex gap-2 items-center"> <FaGithub className="text-[14px] md:text-[25px] dark:text-[#ffff]" /><span className="font-bold dark:text-[#ffff]  text-[12px] md:text-xl text-black">پروژه های متن باز </span></div>
+       
+        <button className="flex items-center gap-2 bg-[#0AADE9] text-white text-[10px] md:text-[16px] leading-7 font-bold py-1 px-3 rounded-[8px]">
+        <CiCirclePlus  className="text-[20px] md:text-[30px]" />
+          افزودن  
+        </button>
+      </div>
+      <div className="flex justify-center items-center flex-col rounded-xl my-10">
+      <AiOutlinePlus className="text-[90px] text-[#999999]" />
+        <span className="text-[#999999] leading-7 font-[700] text-[16px] md:text-2xl">موردی وجود ندارد</span>
+
+      </div>
+      </div>
+    
+
+      {/* پروژه های دیگر */}
+      <div className="border-2 border-[#EEEBEB]  dark:border-[#1B202C] dark:bg-[#1B202C] border-solid  mt-6">
+      <div className="bg-[#EEEBEB]  dark:bg-[#211E1E] p-4 flex justify-between items-center ">
+        <div className="flex gap-2 items-center">   <SiCodecrafters className="text-[14px] md:text-[25px] dark:text-[#ffff]"/><span className="font-bold text-[12px] md:text-xl dark:text-[#ffff] text-black">پروژه های دیگر</span>
+        </div>
+        
+        <button className="flex items-center gap-2 bg-[#0AADE9] text-white text-[10px] md:text-[16px] leading-7 font-bold py-1 px-3 rounded-[8px]">
+        <CiCirclePlus  className="text-[20px] md:text-[30px]" />
+          افزودن  
+        </button>
+      </div>
+      <div className="flex items-center gap-4 p-4">
+        <img
+          src={projeimg}
+          alt="طراحی وبسایت رستوران"
+          className="h-[63px] w-[63px] md:w-[165px] md:h-[165px] rounded-xl object-cover"
+        />
+        <div className="space-y-2 md:space-y-8">
+          <h2 className="font-bold text-[12px] md:text-2xl dark:text-[#FFFFFF]  text-black">طراحی وبسایت رستوران</h2>
+          <p className="text-[#999999] font-medium text-[10px] md:text-lg">تاریخ انتشار : 1404/01</p>
+          <div className="flex gap-4">
+            <span className="bg-[#DADADA] dark:bg-[#323232] text-black dark:text-[#FFFFFF] font-medium  text-[9px] md:text-sm px-3 py-1 rounded-[8px]">Wordpress</span>
+            <span className="bg-[#DADADA] dark:bg-[#323232] text-black dark:text-[#FFFFFF] font-medium  text-[9px] md:text-sm  px-3 py-1 rounded-[8px]">UI/UX</span>
+            <span className="bg-[#DADADA] dark:bg-[#323232] text-black dark:text-[#FFFFFF]  font-medium text-[9px] md:text-sm px-3 py-1 rounded-[8px]">Figma</span>
+          </div>
+        </div>
+        <div className="mr-auto mb-auto flex gap-2">
+          <button className="text-gray-500 hover:text-black"><BiEditAlt  className="text-[12px] md:text-[30px] text-[#8E8E8E]" /></button>
+          <button className="text-red-500 hover:text-red-700"><IoIosCloseCircleOutline  className="text-[12px] md:text-[30px] text"/></button>
+        </div>
+      </div>
+</div>
+    </div>
+
+
           </div>
         </div>
       </div>
--- src/pages/panel/Transactions.jsx
+++ src/pages/panel/Transactions.jsx
@@ -23,30 +23,30 @@ export default function Transactions() {
           <div className="p-5">
             {/*کداتو اینجا بزن*/}
 
-            <div className="max-w-5xl mx-auto p-6 font-yekan text-right">
+            <div className=" mx-auto p-6 font-yekan text-right">
     
 
       {/* کیف پول */}
       <div className="mt-12">
-        <h3 className="text-[16px] md:text-[32px] leading-7 dark:text-[#ffff]  font-bold mb-[20px]">کیف پول</h3>
-        <div className="border-[1px] border-[#CDCACA] mb-8 mt-10 w-full "></div>
+        <h3 className="text-[16px] md:text-[22px] leading-7 dark:text-[#ffff]  font-bold mb-[18px]">کیف پول</h3>
+        <div className="border-[1px] border-[#CDCACA] mb-8 mt-8 w-full "></div>
 
     
           <div className="flex gap-2 items-baseline">
-                <p className="text-[16px] md:text-[32px] leading-7 font-medium dark:text-[#ffff]">
+                <p className="text-[16px] md:text-[22px] leading-7 font-medium dark:text-[#ffff]">
           موجودی:
             </p>
-             <p className=" w-[120px] h-[47px] rounded-[8px] text-center pt-2 bg-[#E8E8E8] dark:bg-[#242424] text-[12px] md:text-[22px] dark:text-[#AAAAAA] leading-[30px] text-[#5D5D5D] font-extrabold">۰ تومان</p>
+             <p className=" w-[70px] h-[35px] rounded-[8px] text-center  bg-[#E8E8E8] dark:bg-[#242424] text-[12px] md:text-[16px] dark:text-[#AAAAAA] leading-[30px] text-[#5D5D5D] font-extrabold">۰ تومان</p>
           </div>
         
       </div>
 
       {/* تراکنش‌ها */}
       <div className="mt-[90px]">
-        <h3 className="text-[14px] md:text-[32px] leading-7 font-medium dark:text-[#ffff]  mb-[30px]">تراکنش‌ها</h3>
+        <h3 className="text-[14px] md:text-[22px] leading-7 font-medium dark:text-[#ffff]  mb-[20px]">تراکنش‌ها</h3>
         {/* عنوان ستون‌ها */}
-        <div className="border-[1px] border-[#CDCACA] mb-6  w-full "></div>
-        <div className="bg-[#EEEBEB] dark:bg-[#211E1E] rounded-t-lg p-3 py-6 grid grid-cols-6 justify-items-center gap-4 dark:text-[#ffff]  text-[16px] md:text-[26px] leading-7 font-bold text-black">
+        <div className="border-[1px] border-[#CDCACA] mb-4  w-full "></div>
+        <div className="bg-[#EEEBEB] dark:bg-[#211E1E] rounded-t-lg p-3 py-6 grid grid-cols-6 justify-items-center gap-4 dark:text-[#ffff]  text-[14px] md:text-[20px] leading-7 font-bold text-black">
           <span>شناسه</span>
           <span>مبلغ</span>
           <span className="col-span-2">زمان</span>
@@ -58,14 +58,14 @@ export default function Transactions() {
         {transactions.map((tx) => (
           <div
             key={tx.id}
-            className="w-full bg-white shadow-md dark:bg-[#1B202C] rounded-b-lg px-6 py-8  grid grid-cols-6 justify-items-center gap-4 dark:text-[#ffff] text-black text-[12px] md:text-[24px]"
+            className="w-full bg-white shadow-md dark:bg-[#1B202C] rounded-b-lg px-6 py-8  grid grid-cols-6 justify-items-center gap-4 dark:text-[#ffff] text-black text-[12px] md:text-[18px]"
           >
             <div>{tx.id}</div>
             <div>{tx.amount} تومان</div>
             <div className="col-span-2">{tx.time}</div>
             <div>{tx.description}</div>
             <div>
-              <p className="bg-[#FBBD08] text-center text-white px-1.5  md:px-4 py-1 rounded-[8px] text-[12px] md:text-[18px]">
+              <p className="bg-[#FBBD08] text-center text-white px-1.5  md:px-4 py-1 rounded-[8px] text-[12px] md:text-[16px]">
                 {tx.status}
               </p>
             </div>
