907d4ba2b41739c9aeabf4665ff26a4c63b6a509
hani <hanirezaee496@gmail.com>
2025-07-23 11:04:34 +0330

    change 2

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,12 +1,46 @@
 import img from '../assets/images/banner-06.png'
+import User from '../assets/images/User.jpg'
 import Basket from '../assets/images/basket.svg'
 import Heart from '../assets/images/heart.svg'
 import Receive from '../assets/images/receive.svg'
 import Save from '../assets/images/save.svg'
 
-export default function ProductDetails(){
-    <div className='p-7'> 
-        <img src={img} alt="" className='w-48'/>
-        <h1>hello</h1>
-    </div>
+export default function ProductDetails() {
+    return (
+        <div className='p-7 space-y-5'>
+            <img src={img} alt="" className='w-full h-60' />
+            <div className='mt-20 flex items-center justify-between'>
+                <div className='flex items-center gap-4'>
+                    <img src={User} alt="" className='w-14 h-14 rounded-full' />
+                    <h1 className='font-IranYekanBold'>سهیل شکریان</h1>
+                </div>
+                <div className='flex gap-3 items-center'>
+                    <img src={Receive} alt="" className='bg-primary p-1 rounded-full size-7'/>
+                    <img src={Heart} alt="" className='bg-primary p-1 rounded-full size-7'/>
+                    <img src={Save} alt="" className='bg-primary p-1 rounded-full size-7'/>
+                </div>
+            </div>
+            <h1>طراحی کارت بانکی</h1>
+            <p className='text-justify text-second-light font-IranYekanBold'>توضیحات بیشتر پروژه ی خود را در این قسمت تایپ کنید توضیحات بیشتر پروژه توضیحات کامل تر درباره پروژه توضیحات کامل تر درباره پروژه توضیحات کامل تر درباره پروژه توضیحات کامل تر درباره پروژه توضیحات کامل تر درباره پروژه</p>
+            <div className='flex gap-2 text-sm text-white'>
+                <p className='bg-second-light px-2 rounded-md'>کامپیوتر</p>
+                <p className='bg-second-light px-2 rounded-md'>طراحی سایت</p>
+                <p className='bg-second-light px-2 rounded-md'>طراحی</p>
+                <p className='bg-second-light px-2 rounded-md'>فتوشاپ</p>
+            </div>
+            <div className='flex justify-between mt-10'>
+                <p className='text-primary font-IranYekanBold'>1/450/000تومان</p>
+                <div className='flex gap-2 items-center bg-primary p-1 rounded-md text-white text-sm'>
+                    <img src={Basket} alt="" className='size-6'/>
+                    سبدخرید
+                </div>
+            </div>
+            <h1 className='mt-20'>توضیحات تکمیلی درباره پروژه</h1>
+            <p className='text-justify text-second-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis libero odit repellat nulla repellendus accusamus dolores doloribus incidunt recusandae, eius soluta, autem perspiciatis ratione sunt? Quos iste nemo eius libero.
+            Nesciunt pariatur fugit magni ad voluptatibus quos architecto consequuntur saepe nemo! Architecto mollitia incidunt reprehenderit nobis, eligendi officiis eos iste asperiores esse natus odio? Numquam expedita qui laboriosam voluptatum ipsum.
+            Harum id voluptate magni quidem modi excepturi alias beatae minus necessitatibus esse itaque iure eum omnis vel nobis, sint nostrum deserunt quo sit inventore autem, in dolore! Officiis, similique alias.
+            </p>
+
+        </div>
+    )
 }
