a98d8ce321bb016c2a15ac34080a899b7c1f0548
MohamadAminPour <ytornado37@gmail.com>
2025-07-29 13:58:07 +0430

    add some change and finish topbar and sidebar (panel)

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,15 +3,15 @@ import { NavLink } from "react-router-dom";
 
 export default function SideBar({ isOpen, setIsOpen }) {
     return (
-        <div className={`${isOpen ? 'translate-x-[0]' : 'translate-x-[20rem]'} w-[15rem] fixed z-10 bg-white p-3 duration-500 `}>
+        <div className={`${isOpen ? 'translate-x-[0]' : 'translate-x-[20rem]'} w-[15rem] fixed z-10 p-3 duration-500 border-l border-zinc-500 bg-white dark:bg-dark dark:text-white text-black`}>
             <button onClick={() => setIsOpen(!isOpen)}>close</button>
             <ul className="*:mb-2 **:w-full **:inline-block  *:rounded-sm *:hover:bg-primaryLight ">
-                <li><NavLink to="/userInfo" className="p-1">اطلاعات فردی</NavLink></li>
-                <li><NavLink to="/skills" className="p-1">مهارت های فنی</NavLink></li>
-                <li><NavLink to="/transactions" className="p-1">تراکنش ها</NavLink></li>
-                <li><NavLink to="/projects" className="p-1">پروژه ها</NavLink></li>
-                <li><NavLink to="/achievements" className="p-1">دستاورد ها</NavLink></li>
-                <li><NavLink to="/setting" className="p-1">تنظیمات</NavLink></li>
+                <li><NavLink to="/userInfo" className="p-2 text-[.9rem] ">اطلاعات فردی</NavLink></li>
+                <li><NavLink to="/skills" className="p-2 text-[.9rem] ">مهارت های فنی</NavLink></li>
+                <li><NavLink to="/transactions" className="p-2 text-[.9rem] ">تراکنش ها</NavLink></li>
+                <li><NavLink to="/projects" className="p-2 text-[.9rem] ">پروژه ها</NavLink></li>
+                <li><NavLink to="/achievements" className="p-2 text-[.9rem] ">دستاورد ها</NavLink></li>
+                <li><NavLink to="/setting" className="p-2 text-[.9rem] ">تنظیمات</NavLink></li>
             </ul>
         </div>
     )
