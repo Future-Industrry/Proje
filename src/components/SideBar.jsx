1c02c69700b2b0c8bea8343a11a7e8353e0b9be0
MohamadAminPour <ytornado37@gmail.com>
2025-08-07 13:38:18 +0430

    add some change to panel

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,12 +6,12 @@ export default function SideBar({ isOpen, setIsOpen }) {
         <div className={`${isOpen ? 'translate-x-[0]' : 'translate-x-[20rem]'} w-[15rem] fixed z-10 p-3 duration-500 border-l border-zinc-500 bg-white dark:bg-dark dark:text-white text-black`}>
             <button onClick={() => setIsOpen(!isOpen)}>close</button>
             <ul className="*:mb-2 **:w-full **:inline-block  *:rounded-sm *:hover:bg-primaryLight ">
-                <li><NavLink to="/userInfo" className="p-2 text-[.9rem] ">اطلاعات فردی</NavLink></li>
-                <li><NavLink to="/skills" className="p-2 text-[.9rem] ">مهارت های فنی</NavLink></li>
-                <li><NavLink to="/transactions" className="p-2 text-[.9rem] ">تراکنش ها</NavLink></li>
-                <li><NavLink to="/projects" className="p-2 text-[.9rem] ">پروژه ها</NavLink></li>
-                <li><NavLink to="/achievements" className="p-2 text-[.9rem] ">دستاورد ها</NavLink></li>
-                <li><NavLink to="/setting" className="p-2 text-[.9rem] ">تنظیمات</NavLink></li>
+                <li><NavLink to="/panel/userInfo" className="p-2 text-[.9rem] ">اطلاعات فردی</NavLink></li>
+                <li><NavLink to="/panel/skills" className="p-2 text-[.9rem] ">مهارت های فنی</NavLink></li>
+                <li><NavLink to="/panel/transactions" className="p-2 text-[.9rem] ">تراکنش ها</NavLink></li>
+                <li><NavLink to="/panel/projects" className="p-2 text-[.9rem] ">پروژه ها</NavLink></li>
+                <li><NavLink to="/panel/achievements" className="p-2 text-[.9rem] ">دستاورد ها</NavLink></li>
+                <li><NavLink to="/panel/setting" className="p-2 text-[.9rem] ">تنظیمات</NavLink></li>
             </ul>
         </div>
     )
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -7,7 +7,7 @@ export default function TopBar({ isOpen, setIsOpen }) {
         <div className="h-[6rem] flex items-center gap-5 p-5 bg-white dark:bg-dark">
             <HiMenu onClick={() => setIsOpen(!isOpen)} className="bg-zinc-200 dark:bg-zinc-800 dark:text-white p-1 rounded-lg text-[2rem] cursor-pointer hover:scale-110 duration-150 " />
             <div className="flex items-center gap-3 ">
-                <img src="public/images/team3.jpg" alt="" className="size-[3.5rem] rounded-full object-cover " />
+                <img src="/images/team3.jpg" alt="" className="size-[3.5rem] rounded-full object-cover " />
                 <div>
                     <div className="flex items-center gap-3 ">
                         <p className='dark:text-white text-black'>Mamad2091</p>
